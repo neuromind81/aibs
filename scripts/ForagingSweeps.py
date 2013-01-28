a74c3de987994697e8bb18a144b8b9b13d1b6b7e
Derric Williams <derricw@gmail.com>
2013-01-28 11:11:43 -0800

    ForagingSweeps
    sample of Current foraging script being used.

--- scripts/ForagingSweeps.py
+++ scripts/ForagingSweeps.py
@@ -17,15 +17,15 @@ d = DynamicParams()
 # MOUSE ID
 s.mouseid = 'test'
 # pre-experiment duration to display blank screen (sec)
-s.preexpSec = 1
+s.preexpSec = 10
 # post-experiment duration to display blank screen (sec)
 s.postexpSec = 1
 # bar orientation offset (deg)
 s.orioff = 0 #dc.get('Manbar0', 'orioff')
 # grating width (deg)
-s.widthDeg = 150 #dc.get('Manbar0', 'widthDeg')
+s.widthDeg = 175 #dc.get('Manbar0', 'widthDeg')
 # grating height (deg)
-s.heightDeg = 120 #dc.get('Manbar0', 'heightDeg')
+s.heightDeg = 150 #dc.get('Manbar0', 'heightDeg')
 # mask, one of:  None, 'gaussian', or 'circle'
 s.mask = None #'gaussian'
 # screen gamma: None, or single value, or 3-tuple
@@ -33,26 +33,30 @@ s.gamma = None
 # starting y position of terrain
 s.ypos = 500
 # set up wheel encoder (NIDAQ Device, Vin channel, Vsig channel)
-s.encoder = Encoder('Dev1',1,2)
+s.encoder = Encoder('Dev2',1,2)
+# sync square
+s.syncsq = False
+# sync square location
+s.syncsqloc = (100,1000)
 
 # set up terrain? [trainingtypes] for example ["color"]
 s.terrain = Terrain(["color", "orientation"])
 # define terrain parameters
 s.terrain.color = s.terrain.white #(0-1) starting color
 s.terrain.correctfreq = 1 #(0-1)
-s.terrain.orientation = 45 # starting orientation
-s.terrain.lapdistance = 1920 # distance in pixels between stimuli (should be wider than screen)
+s.terrain.orientation = 0 # starting orientation
+s.terrain.lapdistance = 3500 # distance in pixels between stimuli (should be wider than screen)
 s.terrain.windowwidth = 200 # size in pixels of "correct" region
-s.terrain.selectiontime = 30 # in frames 
+s.terrain.selectiontime = 5 # in frames 
 s.terrain.speedgain = 4 # no units
 s.terrain.colorrandom = True #randomize the stimulus color
 s.terrain.colorcorrect = s.terrain.black #what is the correct color
-s.terrain.objectwidthDeg = 20 #width of the object in degrees
+s.terrain.objectwidthDeg = 23 #width of the object in degrees
 
 #set up reward (NIDAQ device, port for Digital Out, number of lines, rewardline)
-s.reward = Reward('Dev1',1,0)
+s.reward = Reward('Dev2',1,0)
 #define reward paramters
-s.reward.rewardtime = 0.04
+s.reward.rewardtime = 0.03
 
 #===============================================================================
 # Dynamic parameters can potentially vary from one sweep to the next. If a dynamic parameter 
@@ -69,7 +73,7 @@ d.yposDeg = 0
 # mask diameter (deg), ignored if mask is None
 d.diameterDeg = 10
 # spatial frequency (cycles/deg)
-d.sfreqCycDeg = [0.2,0.5,1]
+d.sfreqCycDeg = [0.1,0.2]
 # temporal frequency (cycles/sec)
 d.tfreqCycSec = [1,2,3]
 # grating phase to begin each sweep with (+/- deg)
@@ -81,9 +85,9 @@ d.contrast = 1.5
 # background brightness (0-1)
 d.bgbrightness = 0.5
 # sweep duration (sec)
-d.sweepSec = 1
+d.sweepSec = 5
 # post-sweep duration to display blank screen (sec)
-d.postsweepSec = 2
+d.postsweepSec = 0
 # reverse the contrast?
 d.contrastreverse = False
 # contrast reverse freq?
@@ -107,7 +111,7 @@ vs.tfreqCycSec = Variable(vals=d.tfreqCycSec, dim=2, shuffle=False)
 
 runs = Runs(n=4, reshuffle=False)
 
-bs = BlankSweeps(T=2000, sec=2, shuffle=False) # blank sweep every T sweeps for sec seconds
+bs = BlankSweeps(T=7, sec=5, shuffle=False) # blank sweep every T sweeps for sec seconds
 
 e = ForagingSweeps(script=__file__, # this script's file name
             static=s, dynamic=d, variables=vs,
