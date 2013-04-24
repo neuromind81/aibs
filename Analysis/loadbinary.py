20b80b9ada0e2c6fdf02166d1658bc451e0a7167
Derric Williams <derricw@gmail.com>
2013-04-23 18:03:46 -0700

    loadbinary
    awesome

---
+++ Analysis/loadbinary.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 14:06:36 2013
+
+@author: derricw
+"""
+import numpy as np
+from pylab import *
+
+def loadbinary(path, channels = 1, dtype = np.int16):
+    f = open(path)
+    data = np.fromfile(f,dtype)
+    size = len(data)
+    del data
+    f.close()
+    print "Data length:", size
+    print "Channels:",channels
+
+    data = np.memmap(path, dtype=dtype,mode='r',shape=((size/channels),channels))
+    return data
+
+def loadmeta(path):
+    m = open(metapath)
+    meta = m.readlines()
+    channels = int(meta[7].split(' = ')[1])
+    samplerate = int(meta[10].split(' = ')[1])
+    
+    for item in meta:
+        print item.strip()
+    return channels, samplerate
+
+if __name__ == "__main__":
+    """
+    datapath = r"C:\Users\derricw\Documents\data\20130205_test7.dat"
+    metapath = r"C:\Users\derricw\Documents\data\20130205_test7.meta"
+    """    
+    datapath = r"C:\Users\derricw\Documents\data\2013_03_14_M14_SPARSE1.dat"
+    metapath = r"C:\Users\derricw\Documents\data\2013_03_14_M14_SPARSE1.meta"
+    
+       
+    channels,_ = loadmeta(metapath)
+    data = loadbinary(datapath, channels=channels)
+
+    #Plot a small section of the data.
+    f = figure(0)
+    subset = np.array(data[39.2e6:39.30e6,64:67])
+    plot(subset)
+    xlabel('Sample')
+    ylabel('Voltage')
+    legend(['Sweep','Frame','Diode','Signal'])
+    title('All Channels')
+    show()
